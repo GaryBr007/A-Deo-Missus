d1bd20615611445180752f7942714bd60196e780
GaryBr007 <icarogarybr007@gmail.com>
2026-06-17 15:23:43 -0300

    Update tela_menu.gd

--- A Deo Missus 2D Alpha/scripts/cenas/tela_menu.gd
+++ A Deo Missus 2D Alpha/scripts/cenas/tela_menu.gd
@@ -1,20 +1,21 @@
 extends Node2D
  
-@onready var recomendacao_texto = $RecomendacaoTexto
-@onready var recomendacao_seta = $RecomendacaoSeta
+#@onready var recomendacao_texto = $RecomendacaoTexto
+#@onready var recomendacao_seta = $RecomendacaoSeta
 
 func _ready() -> void:
 	$AnimationPlayer.play("FadeIn")
 	Global.speedrun_time = 0
-	if Global.tutorial == true:
-		recomendacao_texto.visible = false
-		recomendacao_seta.visible = false
+#	if Global.tutorial == true:
+#		recomendacao_texto.visible = false
+#		recomendacao_seta.visible = false
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://cenas/fases/antartica.tscn")
-	
-func _on_comojoga_pressed() -> void:
-	get_tree().change_scene_to_file("res://cenas/menus/comojoga.tscn")
+	if Global.tutorial == true:
+		get_tree().change_scene_to_file("res://cenas/fases/antartica.tscn")
+	else:
+		get_tree().change_scene_to_file("res://cenas/menus/comojoga.tscn")
+
 	
 func _on_credits_pressed() -> void:
 	get_tree().change_scene_to_file("res://cenas/menus/creditos.tscn")
