6ab0a21b9489b6359fd188404ccbfcc7b3163942
GaryBr007 <icarogarybr007@gmail.com>
2026-06-18 17:20:45 -0300

    ipuupidsh

--- A Deo Missus 2D Alpha/scripts/misc/entrada.gd
+++ A Deo Missus 2D Alpha/scripts/misc/entrada.gd
@@ -1,11 +1,14 @@
 extends Area2D
 
+var somdoinferno = false
+
+
 func _process(_delta: float) -> void:
-	if len(Global.itens) >= 1:
+	if len(Global.itens) >= 1 and monitorable == false:
 		visible = true
 		monitoring = true
 		monitorable = true
-		
+		$AudioStreamPlayer2D3.play()
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		get_tree().change_scene_to_file("res://cenas/fases/cenaro.tscn")
